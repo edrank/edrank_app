95bc94e627fe04506d53f31fce96eecacb1c8a5e
Rishi dholekheria <rishidholkeheria2001@gmail.com>
2022-08-08 13:33:06 +0530

    Scoreboard

--- app/src/main/java/com/example/edrank_app/ui/teacher/ScorecardFragment.kt
+++ app/src/main/java/com/example/edrank_app/ui/teacher/ScorecardFragment.kt
@@ -1,5 +1,7 @@
 package com.example.edrank_app.ui.teacher
 
+import android.graphics.Color
+import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,10 +9,15 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.edrank_app.databinding.FragmentHomeParentBinding
 import com.example.edrank_app.databinding.FragmentScorecardBinding
+import nl.dionsegijn.konfetti.KonfettiView
+import nl.dionsegijn.konfetti.models.Size
 
 class ScorecardFragment : Fragment() {
     private var _binding: FragmentScorecardBinding? = null
     private val binding get() = _binding!!
+
+//    private lateinit var cel: KonfettiView
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,7 +25,22 @@ class ScorecardFragment : Fragment() {
     ): View? {
         _binding = FragmentScorecardBinding.inflate(inflater, container, false)
 
+        binding.celeBtn .setOnClickListener(View.OnClickListener {
 
+            binding.celebrateView.build()
+                .addColors(Color.RED, Color.BLUE, Color.YELLOW, Color.GREEN)
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 4f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(2000L)
+                .addShapes(
+                    nl.dionsegijn.konfetti.models.Shape.CIRCLE,
+                    nl.dionsegijn.konfetti.models.Shape.RECT
+                )
+                .addSizes(Size(12, 5F))
+                .setPosition(-50f, binding.celebrateView.getWidth() + 50f, -50f, -50f)
+                .streamFor(300, 5000L)
+        })
 
 
         return binding.root
