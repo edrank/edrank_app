18ac627c60981f5e3e3300ea4a98305c3231c7f6
Rishi dholekheria <rishidholkeheria2001@gmail.com>
2022-08-12 20:38:11 +0530

    Teachers UI - scorecard and home page

--- app/src/main/java/com/example/edrank_app/MainActivity.kt
+++ app/src/main/java/com/example/edrank_app/MainActivity.kt
@@ -2,13 +2,24 @@ package com.example.edrank_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.edrank_app.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
 
     }
 }
